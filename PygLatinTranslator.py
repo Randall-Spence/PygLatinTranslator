8301b78f6281daa730fadd5b02b492ec91a559e0
Randall-Spence <randall.spence1@gmail.com>
2014-03-15 00:04:06 -0400

    Update PygLatinTranslator.py

--- PygLatinTranslator.py
+++ PygLatinTranslator.py
@@ -1,25 +1,25 @@
-pyg = 'ay'
 #Variable used to add the 'ay' on the end of the translated word.
+pyg = 'ay'
 
-original = raw_input('Enter a word:')
 #Stores user input as the original variable.
+original = raw_input('Enter a word:')
 
-if len(original) > 0 and original.isalpha():
 #Test used to ensure that user inputs alpha characters only.
+if len(original) > 0 and original.isalpha():
 
+    #Prints original variable, stores all lower case version of original variable as word variable.
     print original
     word = original.lower()
-    #Prints original variable, stores all lower case version     of original variable as word variable.
 
+#If the "if" conditions are not met, prints empty and program terminates.
 else:
     print 'empty'
-    #If the "if" conditions are not met, prints empty and         program terminates.
     
-first = word[0]
 #Stores the first letter of word variable as first variable.
+first = word[0]
 
-new_word = word[1:len(word)] + first + pyg
 #new_word variable is created by combining all characters minus the first of word variable, first variable, and pyg variable.
+new_word = word[1:len(word)] + first + pyg
 
-print new_word
 #Displays new_word variable.
+print new_word
